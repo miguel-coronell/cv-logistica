33571b5490aa32322c07b2cda9aceb325fccc37f
miguel coronell <miguelcoronell94@gmail.com>
2026-02-18 18:51:48 -0300

    Actualizacion , se creo centro de descargas y algunos titulos

--- assets/vendor/aos/aos.cjs.js
+++ assets/vendor/aos/aos.cjs.js
@@ -106,7 +106,7 @@ var _extends = Object.assign || function (target) {
 };
 
 /**
- * Device detector
+ * Detector de dispositivos
  */
 
 var fullNameRe = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i;
